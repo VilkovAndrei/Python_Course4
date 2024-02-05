e9f26eb1de117f0f3827d5ca2e7b6f25f9b5eacd
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-02-06 00:28:41 +0300

    Add src/savers.py

---
+++ src/savers.py
@@ -0,0 +1,67 @@
+from abc import abstractmethod, ABC
+import os.path
+import json
+from config import ROOT_DIR
+from vacancy import Vacancy
+
+
+class JobProcessing(ABC):
+    """Абстрактный класс для работы с файлом с вакансиями"""
+
+    @abstractmethod
+    def insert(self):
+        """Записывает информацию в файл"""
+        pass
+
+    @abstractmethod
+    def select(self):
+        """Открывает для чтения информацию из файла.
+           Возвращает список объектов класса вакансий.
+        """
+        pass
+
+    @abstractmethod
+    def delete_info(self):
+        """ Удаляет информацию о вакансиях из файла """
+        pass
+
+
+class JSONSaver(JobProcessing):
+    """Класс для работы с json-файлом с вакансиями"""
+
+    def __init__(self, keyword):
+        self.filename = os.path.join(ROOT_DIR, "data", keyword.title() + ".json").replace('\\','/')
+        self.vacancies_json = []
+        self.vacancies = []
+
+    def insert(self, vacancies_json):
+        """Записывает информацию в файл"""
+        self.vacancies_json = vacancies_json
+
+        with open(self.filename, "w", encoding="utf-8") as file:
+            json.dump(self.vacancies_json, file, indent=4, ensure_ascii=False)
+
+    def select(self):
+        """Открывает для чтения информацию из файла.
+           Возвращает список объектов класса вакансий.
+        """
+        with open(self.filename, "r", encoding="utf-8") as file:
+            vacancies = json.load(file)
+        if vacancies:
+            return [Vacancy(**x) for x in vacancies]
+        return []
+
+    def sorted_by_salary(self):
+        """Возвращает отсортированный по минимальной зарплате список объектов класса вакансий"""
+
+        with open(self.filename, "r", encoding="utf-8") as file:
+            vacancies = json.load(file)
+        self.vacancies = [Vacancy(**x) for x in vacancies]
+        self.vacancies.sort(reverse=True)
+
+        return self.vacancies
+
+    def delete_info(self):
+        """Удаляет информацию в json-файле с вакансиями"""
+        with open(self.filename, "w", encoding="utf-8") as file:
+            json.dump([], file, indent=4, ensure_ascii=False)
